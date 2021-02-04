9e8dd6dda6beccc0829a3e6cd54a03df3b12b1ff
yoanmarcos <yomarcos@free.fr>
2021-02-04 10:45:13 +0100

    make efficient Router and NavBar,create 3 new pages

---
+++ src/Pages/Map.jsx
@@ -0,0 +1,5 @@
+const Map = () => {
+  return <h1>Et ici une carte</h1>;
+};
+
+export default Map;
---
+++ src/Pages/Ressource.jsx
@@ -0,0 +1,5 @@
+const Ressource = () => {
+  return <h1>Kek Chose</h1>;
+};
+
+export default Ressource;
